d06e739ee263381acc231c91de5962fe6a54f10b
David Ortega <a01286040@tec.mx>
2025-08-26 21:37:24 -0600

    Agregar modelo Pokemon con propiedades básicas y respuesta de la API

--- PokemonApi/Models/Pokemon.swift
+++ PokemonApi/Models/Pokemon.swift
@@ -5,3 +5,30 @@
 //  Created by David Ortega Muzquiz on 26/08/25.
 //
 
+import Foundation
+
+// Respuesta del listado
+struct PokemonResponse: Decodable {
+    let results: [PokemonEntry]
+}
+
+// Cada pokémon de la lista
+struct PokemonEntry: Decodable, Identifiable {
+    let name: String
+    let url: String
+    
+    var id: String { url }
+}
+
+// Detalle de un pokémon (para imagen, peso, altura, etc.)
+struct PokemonDetail: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let height: Int
+    let weight: Int
+    let sprites: Sprites
+    
+    struct Sprites: Decodable {
+        let front_default: String?
+    }
+}
